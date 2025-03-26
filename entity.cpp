ee60ec97925c1dcd2708b1941c7a00f388ea196a
mentalicurse <121573729+mentalicurse@users.noreply.github.com>
2025-03-26 13:20:41 +0700

    Update entity.cpp

--- entity.cpp
+++ entity.cpp
@@ -24,24 +24,29 @@ public:
 
 class AccountRepository :public IRepository {
     void Save(AbstractEntity* entity) override {
+    if (typeid(*entity) == typeid(AccountEntity))
         cout << "специфические действия для AccountEntity" << endl;
-    }
+    else cout << "неверный входной параметр, нужен тип AccountEntity,а используется " << typeid(*entity).name() <<endl;
+}
 };
 
 class RoleRepository :public IRepository {
     void Save(AbstractEntity* entity) override {
+    if (typeid(*entity) == typeid(RoleEntity))
         cout << "специфические действия для RoleEntity" << endl;
-    }
+    else cout << "неверный входной параметр, нужен тип RoleEntity,а используется "<< typeid(*entity).name() <<endl;
+}
 };
 
 int main() {
     setlocale(LC_ALL, "Rus");
-    AccountEntity entity;
-    RoleEntity ent;
+    AccountEntity entity;    
+    RoleEntity role;
     IRepository* rep_acc = new AccountRepository();
     IRepository* rep_role = new RoleRepository();
+    rep_acc->Save(&role);
     rep_acc->Save(&entity);
-    rep_role->Save(&ent);
+    rep_role->Save(&role);
     delete rep_acc;
     delete rep_role;
     return 0;
